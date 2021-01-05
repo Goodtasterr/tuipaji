bb586eb5876205daff58df6b304751295c07340f
hwq <Goodtasterr@github.com>
2021-01-05 09:40:24 +0800

    fix to class

--- point_sealevel.py
+++ point_sealevel.py
@@ -31,6 +31,98 @@ def quat2rotation(Q):
 
     return np.asarray(R).astype(np.float32)
 
+class tuipaji_pc():
+    def __init__(self,pcd_root,quat,rotation_x=None):
+        super().__init__()
+        self.quat2rota = quat2rotation(quat)
+        self.point_cloud_origin = np.asarray(o3d.io.read_point_cloud(pcd_root).points)
+        self.point_cloud = np.dot(self.point_cloud_origin,self.quat2rota)
+        trans = [[0, 1, 0],
+                 [0, 0, 1],
+                 [-1, 0, 0]]
+        self.point_cloud = np.dot(self.point_cloud, trans)
+        if rotation_x is not None:
+            theta_x = rotation_x/180*np.pi
+            rotation_matrix = [[1, 0, 0],
+                          [0, np.cos(theta_x), -np.sin(theta_x)],
+                          [0, np.sin(theta_x), np.cos(theta_x)]]
+            self.point_cloud = np.dot(self.point_cloud,rotation_matrix)
+        self.high = [np.min(self.point_cloud[:, 0]), np.max(self.point_cloud[:, 0])]
+        print('down-->up:',self.high)
+        self.width = [np.min(self.point_cloud[:, 1]), np.max(self.point_cloud[:, 1])]
+        print('left-->right:',self.width)
+        self.long = [np.min(self.point_cloud[:, 2]), np.max(self.point_cloud[:, 2])]
+        print('front-->back:',self.long)
+
+    def o3d_show(self,delta_h=None):
+        mesh = o3d.geometry.TriangleMesh.create_coordinate_frame()
+        mesh.scale(10, center=mesh.get_center())
+        if delta_h is not None:
+            high_steps = []
+            for i in range((int(self.high[1] / delta_h) - int(self.high[0] / delta_h)) + 3):
+                high_steps.append(i * delta_h + delta_h * int(self.high[0] / delta_h - 1))
+            print('contour  high:', high_steps)
+            delta_x = 0.04
+            indexs = np.zeros((self.point_cloud.shape[0])).astype(np.int)
+            for j, high_step in enumerate(high_steps):
+                index_step = (self.point_cloud[:, 0] > high_step - delta_x) & (
+                            self.point_cloud[:, 0] < high_step + delta_x).astype(np.int)
+                indexs += (index_step * (j % 3 + 1))
+
+        pcd_trans = o3d.geometry.PointCloud()
+        pcd_trans.points = o3d.utility.Vector3dVector(self.point_cloud)
+        if delta_h is not None:
+            pcd_trans.colors = o3d.utility.Vector3dVector(pc_colors(indexs))
+        o3d.visualization.draw_geometries([mesh,pcd_trans],window_name='window_name',
+                                       width=1080, height=1080)
+
+    def elevation_map(self,resolution = 0.1,show=''):
+        width_steps = []
+        for i in range((int(self.width[1] / resolution) - int(self.width[0] / resolution)) + 3):
+            width_steps.append(i * resolution + resolution * int(self.width[0] / resolution - 1))
+        print('contour width:', len(width_steps))
+        long_steps = []
+        for i in range((int(self.long[1] / resolution) - int(self.long[0] / resolution)) + 3):
+            long_steps.append(i * resolution + resolution * int(self.long[0] / resolution - 1))
+        print('contour  long:', len(long_steps))
+        bias_width = abs(int(self.width[0] / resolution)) + 1
+        bias_long = abs(int(self.long[0] / resolution)) + 1
+        print('bias width & long:', bias_width, bias_long)
+
+        pixel_points = (self.point_cloud[:, 1:] / resolution).astype(np.int) - 1
+        pixel_points[:, 0] += bias_width
+        pixel_points[:, 1] += bias_long
+        bias_step = 5
+        map_step = np.zeros([len(width_steps), len(long_steps)]) - bias_step
+        for i in range(pixel_points.shape[0]):
+            if (self.point_cloud[i][0] > map_step[pixel_points[i, 0], pixel_points[i, 1]]):
+                map_step[pixel_points[i, 0], pixel_points[i, 1]] = self.point_cloud[i][0]
+        xyh =[]
+        for j in range((map_step.shape[0])):
+            for k in range((map_step.shape[1])):
+                xyh.append((j,k,map_step[j,k]))
+        xyh_np = np.asarray(xyh).astype(np.float32)
+        idx_empty = xyh_np<0.1
+        xyh_false = xyh_np[idx_empty]
+        xyh_true = xyh_np[(~idx_empty)]
+
+
+        print(((map_step + bias_step) * 13).max(), ((map_step + bias_step) * 10).min())
+        map_step2int = ((map_step + bias_step) * 13).astype(np.uint8)
+        if show is 'map':
+            map2img = Image.fromarray(map_step2int)
+            map2img.show()
+        elif show is '':
+            pool_idx = map_step2int < 0.1
+            map_pool = map_step2int + 0.0001
+            map_show = map_step2int + map_pool * pool_idx
+            print('map_show value:', ((map_pool).max(), (map_pool).min()))
+            sns.set()
+            ax = sns.heatmap(map_show, cmap='seismic', center=0, robust=True)
+            plt.axis('equal')
+            plt.show()
+        return map_step
+
 def tuipaji():
     mesh = o3d.geometry.TriangleMesh.create_coordinate_frame()
     mesh.scale(10, center=mesh.get_center())
@@ -48,12 +140,20 @@ def tuipaji():
     pcd = o3d.io.read_point_cloud("./1222/1608617787805101.pcd")
     pcd_points = np.asarray(pcd.points)
 
-    trans = [[0,1,0],[0,0,1],[-1,0,0]] #这里8种都试过了，不是就这个效果么-》
+    trans = [[0,1,0],
+             [0,0,1],
+             [-1,0,0]]
+    theta_x = -20/180*np.pi
+    rotation_x =[[1,0,0],
+                [0,np.cos(theta_x),-np.sin(theta_x)],
+                [0,np.sin(theta_x),np.cos(theta_x)]]
     pcd_points_trans = np.dot(pcd_points,(np.asarray(RR)))
     pcd_points_trans = np.dot(pcd_points_trans,np.asarray(trans))
+    pcd_points_trans = np.dot(pcd_points_trans, np.asarray(rotation_x))
+
     points_number = pcd_points.shape[0]
     pcd.colors = o3d.utility.Vector3dVector(np.repeat([[0,1,1]],points_number,axis=0))
-# 这图片怎么关
+
     #delta_h, delta_x
     delta_h = 1
     delta_x = 0.04
@@ -78,7 +178,7 @@ def tuipaji():
     print('contour  long:', len(long_steps))
     bias_width = abs(int(width[0]/resolution))+1
     bias_long = abs(int(long[0] / resolution)) +1
-    print('bias width long:',bias_width,bias_long)
+    print('bias width & long:',bias_width,bias_long)
 
     import time
     t0 = time.time()
@@ -97,13 +197,14 @@ def tuipaji():
     kernel = np.ones((7, 7), np.float32) / 49
     pool_idx = map_step2int<0.1
     map_pool = map_step2int+0.0001
-    for i in range(4):
-        map_pool = cv.filter2D(map_pool, -1, kernel)
+    # for i in range(4):
+    #     map_pool = cv.filter2D(map_pool, -1, kernel)
     map_show = map_step2int + map_pool*pool_idx
     print('map_show value:',((map_pool).max(), (map_pool).min()))
     sns.set()
     ax = sns.heatmap(map_show,cmap='seismic',center=0,robust=True)
-    plt.show()#这行显示，运行时怎么关闭那个窗口
+    plt.axis('equal')
+    plt.show()
 
     print(type(map_step2int),map_step2int.shape)
     map2img = Image.fromarray(map_step2int)
@@ -127,10 +228,8 @@ def tuipaji():
     # o3d.visualization.draw_geometries([mesh,pcd_trans],window_name='window_name',
     #                                    width=1080, height=1080)
 if __name__ == '__main__':
-    tuipaji()
-    # im = Image.open('./map.pgm')
-    #
-    # im_arr = np.asarray(im)
-    # im_arr_toimg = Image.fromarray(im_arr)
-    # im_arr_toimg.show()
-    # print(type(im_arr),im_arr.shape)
+    # tuipaji()
+
+    Q = np.asarray((-0.091, -0.108, -0.797, 0.588)).astype(np.float32)
+    test = tuipaji_pc(pcd_root="./1222/1608617787805101.pcd",quat=Q,rotation_x=-20)
+    test.elevation_map(show='')
